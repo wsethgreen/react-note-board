5180d0c27e9a3820bd3fc0c28ea8499674d40cdd
wsethgreen <wsethgreen@gmail.com>
2021-06-16 10:52:01 -0400

    additional code/functionality

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,44 +5,40 @@ import Note from './Note';
 
 function Board() {
 
-    const [notes, setNotes] = useState([
-        {
-        title: "Class Notes",
-        body: "Always use constructors when extending another class"
-    },
-    {
-        title: "My New Address",
-        body: "2001 N Lonhill Phoenix, AZ 81234"
-    },
-    {
-        title: "React Notes",
-        body: "Everything in React is a component"
-    }]);
-    
-    const addNote = () => {
-        notes.push({
-            title: "New Note",
-            bode: "This is my new note."
-        })
+    const [notes, setNotes] = useState([]);
 
-        setNotes({notes:notes})
+    // Add button
+    const addNote = () => {
+        notes.push({id: Date.now});
+        setNotes([notes]);
     }
 
-    // const handleEdit = () => {
-    //     setEditMode()
-    // }
+    // Delete button
+    const handleDelete = (id) => {
+        let newNoteArr = notes;
+        newNoteArr.map((note, idx) => {
+            if (id === note.id) {
+                newNoteArr.splice(idx, 1);
+            }
+        })
+        setNotes(newNoteArr);
+    }
 
+    // Return statement
     return (
         <div>
             <div className="div-board">
                 <div className="row">
                     {notes.map(note => {
-                        return <Note title={note.title} body={note.body} />
+                        return <Note 
+                                title={note.title} 
+                                body={note.body} 
+                                handleDelete={handleDelete}/>
                     })}
                 </div>
             </div>
             <div>
-                <button className="btn btn-success add-button" onClick={addNote}>Add</button>
+                <button className="btn btn-success add-button" onClick={addNote}>Add Note</button>
             </div>
         </div>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,25 +1,70 @@
-import React, {useState} from 'react'
+import React, {createRef, useState} from 'react'
 import './Note.css'
+import PropType from 'prop-types'
 
 function Note(props) {
 
-    const [title, setTitle] = useState(props.title);
-    const [body, setBody] = useState(props.body);
+    // Variables aka "States" to set Note Title and Body
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
     const [editMode, setEditMode] = useState(false);
 
+    // Edit button
+    const handleEdit = () => {
+        setEditMode(true);
+    }
+    // Save button
+    const handleSave = () => {
+
+        let titleContent = React.createRef();
+        let bodyContent = React.createRef();
+
+        setTitle(titleContent.current.value);
+        setBody(bodyContent.current.value);
+        setEditMode(false);
+    }
+
+    // Add button variables
+    let titleElement, bodyElement, buttonArea; 
+
+    // Edit button if statement
+    if (editMode){
+      titleElement = <textarea className="title-textarea" defaultValue={title}></textarea>;
+      bodyElement = <textarea className="body-textarea" defaultValue={body}></textarea>;
+      buttonArea = <div><button className="btn btn-primary" onClick={handleSave}>Save</button></div>;
+    }
+    else{
+      titleElement = <h5 className="card-title">{title}</h5>;
+      bodyElement = <p>{body}</p>; 
+      buttonArea = <div><button className="btn btn-info" onClick={handleEdit}>Edit</button><button className="btn btn-danger" onClick={props.handleDelete}>Delete</button></div>;
+    }
+
     return (
-    <div className="col-sm-6">
-        <div className="card card-view cardDis">
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p>{body}</p>
-                <button className="btn btn-info">Edit</button>
-                <button className="btn btn-danger">Delete</button>
+        <div className='col-sm-6'>
+            <div className="card card-view">
+                <div className="card-body">
+                {titleElement}
+                {bodyElement}
+                {buttonArea}
+                </div>
             </div>
-        </div>
-    </div>
+ 	    </div>
+
+
+
+    // <div className="col-sm-6">
+    //     <div className="card card-view cardDis">
+    //         <div className="card-body">
+    //             <h5 className="card-title">{title}</h5>
+    //             <p>{body}</p>
+    //             <button className="btn btn-info">Edit</button>
+    //             <button className="btn btn-danger">Delete</button>
+    //         </div>
+    //     </div>
+    // </div>
 
     )
 }
+
 
 export default Note;
